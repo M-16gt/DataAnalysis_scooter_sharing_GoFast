cd2ed8712eb90cd148f543f1bcdf9984412e4ad0
Макс <popiti2345a@gmail.com>
2026-01-01 20:40:25 +0300

    feat(EDA.ipynb): Проведен полный EDA анализ с выводами исправлены ошибки в расчете выручки.

--- utils/EDA.py
+++ utils/EDA.py
@@ -9,19 +9,32 @@ def get_total_price_by_rule(df: pd.DataFrame, rule: str = "ME") -> pd.Series:
         'minute_price': 'max',
         'duration': 'sum',
         'start_ride_price': 'sum',
-        'subscription_fee': 'sum'
+        'user_id': 'nunique', # NOQA
+        'subscription_fee': 'max'
     })
 
-    df["total_price"] = df["minute_price"] * df["duration"] + df["start_ride_price"] + df["subscription_fee"]
+    df["total_price"] = df["minute_price"] * df["duration"] + df["start_ride_price"] + df["subscription_fee"] * df["user_id"]
 
     return df["total_price"]
 
-def check_ttest_1samp(df: pd.DataFrame, popmean, alternative, text: str, _alpha: float = 0.05) -> None:
-    # Сырая версия
+def check_ttest_1samp(df: pd.DataFrame, popmean, alternative, _alpha: float = 0.05) -> None: # NOQA
 
     result = stats.ttest_1samp(df, popmean, alternative=alternative)
 
+    print("T-statistic: {}".format(result.statistic))
+    print("P-value: {}".format(result.pvalue))
+
     if result.pvalue < _alpha:
-        print("Отклоняем нулевую гипотезу о {}.".format(text))
+        print("Отклоняем нулевую гипотезу.")
     else:
-        print("Нету причины отклонения нулевой гипотезы")
+        print("Нету причины отклонения нулевой гипотезы.")
+
+def check_ttest_ind(df1: pd.DataFrame, df2: pd.DataFrame, alternative,  _alpha: float = 0.05) -> None: # NOQA
+    result = stats.ttest_ind(df1, df2, alternative=alternative, equal_var=False)
+    print("T-statistic: {}".format(result.statistic))
+    print("P-value: {}".format(result.pvalue))
+
+    if result.pvalue < _alpha:
+        print("Отклоняем нулевую гипотезу.")
+    else:
+        print("Нету причины отклонения нулевой гипотезы.")
--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,4 @@ from .overview import print_shape_data, print_duplicates, print_categorical_data
 
 from .vizualization import hist_boxplot, scatterplot
 
-from .EDA import get_total_price_by_rule
+from .EDA import get_total_price_by_rule, check_ttest_ind
